22721332635ef980e895070784fbda4cf9bb6ac7
Austin <austinbirch@toadcode.co.uk>
2010-10-10 01:23:13 +0100

    Modified Activity to reflect changes to the layout.

--- src/com/toadcode/andulator/Andulator.java
+++ src/com/toadcode/andulator/Andulator.java
@@ -2,15 +2,26 @@ package com.toadcode.andulator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class Andulator extends Activity {
+public class Andulator extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        //this is the start of the app the rest of the code goes here
-        
-        //this is another useless comment
+        Log.i("event", "onCreate is done.");
+               
     }
+    
+    
+    public void onClick(View v){
+    	//when any button with a registered handler is clicked
+    	Log.i("event", "view clicked");
+    	
+    }
+    
+    
 }
