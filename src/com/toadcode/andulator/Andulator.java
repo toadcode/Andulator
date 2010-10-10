e021c771abd5ce5b38a8a7ac4a96df38b84a4871
Austin <austinbirch@toadcode.co.uk>
2010-10-10 05:42:27 +0100

    Removed unneeded imports

--- src/com/toadcode/andulator/Andulator.java
+++ src/com/toadcode/andulator/Andulator.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.Gravity;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TableLayout;
