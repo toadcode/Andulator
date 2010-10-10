9bc3f02ae6b2f5ee6b9f33e24a6599e989a9d1d7
Austin <austinbirch@toadcode.co.uk>
2010-10-10 01:54:04 +0100

    Working button click detection, rearrange for a more efficient switch case

--- src/com/toadcode/andulator/Andulator.java
+++ src/com/toadcode/andulator/Andulator.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Andulator extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
@@ -13,14 +14,18 @@ public class Andulator extends Activity implements OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         Log.i("event", "onCreate is done.");
-               
+        Button button = (Button)findViewById(R.id.btn0); 
+        button.setOnClickListener(this);
     }
     
     
     public void onClick(View v){
     	//when any button with a registered handler is clicked
     	Log.i("event", "view clicked");
-    	
+    	if (v.getId() == R.id.btn0){
+    		//button 0 was clicked
+    		Log.i("button_click", "button 0 was clicked!");
+    	}
     }
     
     
